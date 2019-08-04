0737af6a9cd528ffbe8bd61c533fb0d303edb0db
JavierDuarteC <javierandresduarte@gmail.com>
2019-08-03 22:12:32 -0500

    Update edit-exercise.component.js

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -81,7 +81,7 @@ export default class EditExercise extends Component{
 
         console.log(exercise)
 
-        axios.post('http://localhost:5000/exercises/update'+this.props.match.params.id,exercise)
+        axios.post('http://localhost:5000/exercises/update/'+this.props.match.params.id,exercise)
             .then(res=>console.log(res.data))
             .catch(err=>console.log(err))
 
